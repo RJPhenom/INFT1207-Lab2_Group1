60e08369d2fc066f1395cb066f21285148c28878
RJ <rj@macklem.ca>
2024-02-11 22:17:06 -0500

    Update Lab2_robert.py
    Added title field to comment header

--- Lab2_robert.py
+++ Lab2_robert.py
@@ -2,6 +2,7 @@
 #   Class:          INFT1207
 #   Authors:        Robert Macklem
 #   Date:           February 11 2024
+#   Title:          Reading List Tracker
 #   File:           Lab1_robert.py
 #   Description:    Reading list program. The program takes in and/or returns
 #                   books from a user's reading list, which is stored in .csv
