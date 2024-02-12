8d2de5c9f6a998bbe586e9d3355adada1b1e6d7e
RJ <rj@macklem.ca>
2024-02-11 22:31:50 -0500

    Create books.csv
    Created .csv file to track reading list and added basic code from template to write books as dicts to .csv to save and track

--- Lab2_robert.py
+++ Lab2_robert.py
@@ -8,4 +8,25 @@
 #                   books from a user's reading list, which is stored in .csv
 #                   format.
 ###############################################################################
+import csv
+
+
+def add_book(reading_list):
+    # Build book to add as dict obj
+    book = {
+        "Title" : input("Enter the title of your book: ")
+        ,"Author" : input("Enter the author of your book: ")
+        ,"Date" : input("Enter the date of your book's publication: ")
+    }
+
+    # Add it to the reading list
+    reading_list.append(book)
+
+    # Write the new dict to the .csv file to track and save the reading list
+    with open('books.csv', 'a', newline='') as file:
+        writer = csv.DictWriter(file, fieldnames=["Title", "Author", "Date"])
+        writer.writerow(book)
+
+    # Print confirmation message on execution complete
+    print("Book added successfully")
 
