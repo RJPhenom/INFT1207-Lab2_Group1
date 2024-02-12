0d355888a01882789fea5c0ccdef6b52c8137710
RJ <rj@macklem.ca>
2024-02-11 23:21:40 -0500

    Update Lab2_robert.py
    Update lab proj file to have main program switch case for user mode options, also added function for reading all books from books.csv

--- Lab2_robert.py
+++ Lab2_robert.py
@@ -10,13 +10,22 @@
 ###############################################################################
 import csv
 
+# CONSTS
 
+# VARS
+
+# FUNCS
+# mandatory linebreaks for asinine pep8 requirements in JetBrain(les)s
+
+
+# Function add_book prompts user input for Title, Author, Date to build a book dict, which is then written to books.csv
+# to track and save that books as part of the reading list
 def add_book(reading_list):
     # Build book to add as dict obj
     book = {
-        "Title" : input("Enter the title of your book: ")
-        ,"Author" : input("Enter the author of your book: ")
-        ,"Date" : input("Enter the date of your book's publication: ")
+        "Title": input("Enter the title of your book: "),
+        "Author": input("Enter the author of your book: "),
+        "Date": input("Enter the date of your book's publication: ")
     }
 
     # Add it to the reading list
@@ -30,3 +39,55 @@ def add_book(reading_list):
     # Print confirmation message on execution complete
     print("Book added successfully")
 
+
+# Function retrieve_books retrieves all books in books.csv, essentially acting as a "print all books in the reading
+# list" function.
+def retrieve_books(reading_list):
+    # Build a dict reader using the .csv
+    with open('books.csv', 'a', newline='') as file:
+        reader = csv.DictReader(file, fieldnames=["Title", "Author", "date"])
+
+        # Print each row using dict struct
+        for row in reader:
+            print(row["Title"], ", by ", row["Author"], ". Published ", row["Date"])
+
+
+# PROGRAM
+# Welcome message
+print("\nWelcome to the Reading List Tracker!\n")
+print("I would be happy to help you store and track all the wonderful books\nyou would like to read!")
+
+# Program main loop
+# Use boolean to keep application running
+application_quit = False
+while not application_quit:
+
+    # Use boolean to loop while in main menu, waiting for valid operation option (selection var)
+    main_menu = True
+    selection = 0
+
+    while main_menu:
+        print("\nMain Menu")
+        print("Please choose from one of the four options:\n\n1:\tAdd Book\n2:\tView Books\n3:\tBook Search\n4:\tQuit")
+
+        try:
+            selection = int(input("\nInput selection: "))
+            if selection in (1, 2, 3, 4):
+                main_menu = False
+            else:
+                print("\n***INVALID SELECTION*** Please enter a number corresponding to the option you wish to select.")
+
+        except:
+            print("\n***INPUT ERROR*** Please enter the number corresponding to the option you wish to select.")
+
+    # Switch execution mode based on selection
+    match selection:
+        case 1:
+            print("\nYou selected 'Add Book!\n")
+        case 2:
+            print("\nYou selected 'View Books!\n")
+        case 3:
+            print("\nYou selected 'Search Book!\n")
+        case 4:
+            print("\nYou selected 'Quit!\nGoodbye...")
+            application_quit = True
