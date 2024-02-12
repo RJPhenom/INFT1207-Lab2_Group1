cb483efd22a926b3ee1738326e77a65596a9b710
RJ <rj@macklem.ca>
2024-02-11 22:15:33 -0500

    Create Lab2_robert.py
    Add .py source file for lab 2 and wrote comment header for program

---
+++ Lab2_robert.py
@@ -0,0 +1,10 @@
+###############################################################################
+#   Class:          INFT1207
+#   Authors:        Robert Macklem
+#   Date:           February 11 2024
+#   File:           Lab1_robert.py
+#   Description:    Reading list program. The program takes in and/or returns
+#                   books from a user's reading list, which is stored in .csv
+#                   format.
+###############################################################################
+
